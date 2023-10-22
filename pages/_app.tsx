651041144d987e71fdd53986849beeaef08bc67d
Perry Raskin <perryraskin@gmail.com>
2023-10-22 13:37:55 -0400

    Testing favicon for iOS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,11 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>raskFin</title>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
         <link
           rel="icon"
           type="image/png"
